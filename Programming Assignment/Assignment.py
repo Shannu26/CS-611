3c3e781f7360de4bce829f11bda3532fa2aa93e6
Sasank Tadepalli <sasank.tadepalli@icloud.com>
2023-11-06 15:32:33 -0800

    Updated Assignment

--- Programming Assignment/Assignment.py
+++ Programming Assignment/Assignment.py
@@ -1,4 +1,4 @@
-def mapCharacters(pattern):
+def mapCharacters(pattern, text):
 	charToIntMap = {}
 	numberOfUniqueCharacters = 0
 
@@ -7,7 +7,11 @@ def mapCharacters(pattern):
 			charToIntMap[char] = numberOfUniqueCharacters
 			numberOfUniqueCharacters += 1
 
-	print(charToIntMap)
+	for char in text:
+		if char not in charToIntMap:
+			charToIntMap[char] = numberOfUniqueCharacters
+			numberOfUniqueCharacters += 1
+
 	return charToIntMap
 
 def buildDFA(pattern, charToIntMap):
@@ -23,7 +27,6 @@ def buildDFA(pattern, charToIntMap):
 		dfa[charToIntMap[pattern[j]]][j] = j + 1
 		X = dfa[charToIntMap[pattern[j]]][X]
 
-	print(dfa)
 	return dfa
 
 def search(text, dfa, charToIntMap):
@@ -32,17 +35,42 @@ def search(text, dfa, charToIntMap):
 	M = len(dfa[0])
 
 	for i in range(N):
+		if text[i] not in charToIntMap: return -1
 		j = dfa[charToIntMap[text[i]]][j]
-		if j == M: return i - M
+		if j == M: return i - M + 1
 	return -1
 
 
 if __name__ == '__main__':
-	pattern = input("Enter the pattern: ")
-	text = input("Enter the text: ")
+	testcases = [
+				{ "pattern": "abcaby", "text": "abxabcabcaby"},
+				{ "pattern": "ABABCABAB", "text": "ABABDABACDABABCABAB"},
+				{ "pattern": "i am sas", "text": "Hello i am sasank"},
+				{ "pattern": "i am sas", "text": "Hello i am bhavana"},
+				{ "pattern": "rry pott", "text": "I am harry potter"},
+				{ "pattern": "zyx", "text": "z"},
+				{ "pattern": "zyx", "text": "xyz"},
+				{ "pattern": "abbbbccccAA", "text": "AAnndjasbfjbasdbhabbbbccccAA"},
+				{ "pattern": "of the rin", "text": "Lord of the rings"},
+				{ "pattern": "game of", "text": "Game of Thrones"}
+				]
 
-	charToIntMap = mapCharacters(pattern)
-	dfa = buildDFA(pattern, charToIntMap)
+	print("-----------------------------------------------------------------------------------------")
 
-	patternIndex = search(text, dfa, charToIntMap)
-	print(patternIndex)
+	for index, testcase in enumerate(testcases):
+		pattern = testcase["pattern"]
+		text = testcase["text"]
+		charToIntMap = mapCharacters(pattern, text)
+		dfa = buildDFA(pattern, charToIntMap)
+
+		patternIndex = search(text, dfa, charToIntMap)
+		resultText = ""
+		if patternIndex == -1: resultText = "The given pattern is not found in the given text."
+		else: resultText = "The given pattern is found in the given text at index " + str(patternIndex)
+
+		print("Test Case - " + str(index + 1))
+		print("\t\tPattern: " + pattern)
+		print("\t\tText: " + text)
+		print("\t\tResult: " + resultText)
+		print("-----------------------------------------------------------------------------------------")
+
